a20cf160ac108f1a71d131e89dd85b6d4b3eaa51
Puja Chaudhury <61319491+catplotlib@users.noreply.github.com>
2020-06-10 23:26:49 +0530

    Update relabel.py

--- relabel.py
+++ relabel.py
@@ -1,15 +1,15 @@
 def change_name():
-    import os,sys
+    import os
     from os import listdir
     variable=input("Enter new file names:\n")
     path = os.getcwd()
     i = 1
     for filename in listdir(path):
+        m=filename.index('.')
+        l=len(filename) 
+        n=l-m
         if(variable==""):
-            os.rename(filename,str(i).zfill(1)+filename[-4:])
-            i+=1
+            os.rename(filename,str(i).zfill(1)+filename[-n:])
         else:
-            os.rename(filename,variable+ str(i).zfill(1)+filename[-4:])
-            i+=1
-
-
+            os.rename(filename,variable+ str(i).zfill(1)+filename[-n:])
+        i+=1
